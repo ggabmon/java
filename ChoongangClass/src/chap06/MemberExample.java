4a5b607608ad8d65c58d2285c1a9f1c2b9c829dd
whwnd <whwnd@DESKTOP-9TOK500>
2023-05-10 11:24:19 +0900

    줴발

--- ChoongangClass/src/chap06/MemberExample.java
+++ ChoongangClass/src/chap06/MemberExample.java
@@ -7,7 +7,7 @@ public class MemberExample {
 		Member member = new Member();
 		member.name = "최하얀";
 		member.age = 23;
-
+		
 		System.out.println("이름은"+member.name);
 		System.out.println("나이는"+member.age);
 	}
