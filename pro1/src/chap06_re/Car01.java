4ac8066dc150c893a2e9e5895b6cb7a0756768c3
whwnd <whwnd@DESKTOP-9TOK500>
2023-05-10 12:40:41 +0900

    1

--- pro1/src/chap06_re/Car01.java
+++ pro1/src/chap06_re/Car01.java
@@ -1,7 +1,30 @@
 package chap06_re;
-
-public class Car01 {
-	// field : data 보관 생성 등등.. 명사적형태의 것들
+//이 아래에 있는 pulic 또한 접근제어자 이다.
+/* 	★field : data 보관 생성 등등.. 명사적형태의 것들. DB의 column과 동일하게 생각하자.
+	>문법
+ 	[접근제어자] [제어자] 타입 필드명[=초기값(생략가능)];
+	[AccessModifier] [Modifier] Type FieldName [];
+	>>내부에서 제시
 	
-	// method : 기능과 동작을 표현 동사적형태의 것들
+	★constructor(생성자) : ▼객체를 생성해준다▼ 필드값의 초기화를 위해 주로 사용된다.
+	>문법
+	[접근제어자] 클래스명(매개변수리스트){}
+	[Access] ClassName(parameterlist){}
+	>>외부에서 제시
+	
+	★method : 기능과 동작을 표현 동사적형태의 것들
+	>문법
+	[접근제어자] [제어자] 리턴유형 메서드명(매개변수리스트){}
+	[AccessModifier] [Modifier] ReturnType MethodName(parameterlist) [];
+*/
+public class Car01 {
+
+	
+	
+	
+	
+	
+	
+	
+	
 }
